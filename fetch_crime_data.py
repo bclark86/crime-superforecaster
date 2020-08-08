977fee40eaeb56bc285312cba3dd66f5cdf139c1
Bryan Clark <bryan.j.clark08@gmail.com>
2020-08-07 22:03:36 -0400

    update .gitignore

---
+++ fetch_crime_data.py
@@ -0,0 +1,30 @@
+import os
+import pandas
+from scraper import load_yaml, query_crime
+
+
+def main():
+
+    config = load_yaml()
+
+    raw_data_path = os.path.join(
+        config['local_data']['dir'],
+        config['local_data']['raw_dir']
+    )
+
+    if not os.path.exists(raw_data_path):
+        os.makedirs(raw_data_path)
+
+    crime_data_ty = query_crime(
+        config['crime_data']['url'],
+        config['crime_data']['current_year_id']
+    )
+
+    crime_data_ty.to_csv(
+        os.path.join(raw_data_path, config['local_data']['current_year_file']),
+        index=False
+    )
+
+
+if __name__ == '__main__':
+    main()
